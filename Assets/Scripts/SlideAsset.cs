e05b60ef2a38cb4e643f8611dd6cb030a62f4adc
TomohiroHibino600 <62923803+TomohiroHibino600@users.noreply.github.com>
2020-10-30 04:03:27 +0900

    [feat]add slide change timeline
    - スライドを切り替えるPlayableTrackをTimeLineに刺せる
    - 球の大小を切り替えられる

---
+++ Assets/Scripts/SlideAsset.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class SlideAsset : PlayableAsset
+{
+    public Sprite image;
+
+    public override Playable CreatePlayable( PlayableGraph graph, GameObject owner ) {
+        //PlayableBehaviourを継承したSlideChangeクラスを元に、PlayableTrack(コマ)を作る
+        var player = ScriptPlayable<SlideChange>.Create( graph );
+        
+        //SlideChangeクラスにあるプロパティを設定
+        var behaviour = player.GetBehaviour();
+        behaviour.slideImage = image;
+        
+        return player;
+    }
+}
---
+++ Assets/Scripts/SlideChange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class SlideChange : PlayableBehaviour
+{
+    public Sprite slideImage;
+    public GameObject spriteObject;
+
+    //タイムライン開始時実行される
+    public override void OnGraphStart( Playable playable ) {
+        base.OnGraphStart( playable );
+        
+        //スライドを表示するGameObjectを取得
+        spriteObject = GameObject.FindGameObjectWithTag("Window");
+    }
+
+    //PlayableTrack(コマ)再生時実行される
+    public override void OnBehaviourPlay( Playable playable, FrameData info ) {
+        
+        //スライドの画像(Sprite)を変更
+        spriteObject.GetComponent<SpriteRenderer>().sprite = slideImage;
+    
+    }
+}
